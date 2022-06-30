ef2d3744951ea22d8cae29d5011a6f885770ec09
plitche <kysu728@naver.com>
2022-06-30 23:35:18 +0900

    수지 요청 - controller - mapping - model 데이터 - view로 리턴 실습

--- BookStore/src/main/java/com/bookstore/cha/plitche/controller/TestController.java
+++ BookStore/src/main/java/com/bookstore/cha/plitche/controller/TestController.java
@@ -7,14 +7,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 	
-	@RequestMapping(value = "/test.plitche")
+	@RequestMapping(value = "/test1/test2")
 	public String test(Model model) {
 		System.out.println("test 성공");
+		System.out.println("test1/test2 성공");
+		
 		
 		model.addAttribute("result", "success");
 		model.addAttribute("result1", "success1");
+		model.addAttribute("test1", "test1/test2 페이지 도착");
 				
-		return "test";
+		return "test2";
+	}
+	
+	@RequestMapping(value = "/test1")
+	public String test1(Model model) {
+		System.out.println("test1 성공");
+		
+		model.addAttribute("yahoo", "test1 도착");
+		model.addAttribute("yahoo1", 111);
+		
+		return "test1";
 	}
 	
 }
